e7cfdb61911fa34181f3f8e1c5e1b90d64e38e93
Muhusin16 <muhusin@zool.in>
2024-08-20 16:58:15 +0530

    Changes

--- config/multerConfig.js
+++ config/multerConfig.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Set the destination directory
+    cb(null, 'uploads/'); 
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + path.extname(file.originalname);
